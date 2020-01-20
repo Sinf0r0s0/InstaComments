fbd65db3e42ae96149c369c796ad504fb0e41c57
whiteFly <32365615+Sinf0r0s0@users.noreply.github.com>
2020-01-20 17:15:56 -0300

    Update instacomments.py

--- instacomments.py
+++ instacomments.py
@@ -2,7 +2,6 @@
 import re
 import requests
 
-
 """
     InstaComments 1.0
 
@@ -22,7 +21,7 @@ class InstaComments:
     tag_to_re_b_i = 'queryId:"'
     tag_to_re_b_f = '",queryParams'
 
-    def __init__(self, uri, max_com=49, timeout=10):
+    def __init__(self, uri, max_com=40, timeout=10):
         self.uri = uri
         self.max_com = max_com
         self.counter = 0
@@ -32,27 +31,27 @@ class InstaComments:
         # self.dict_response = {}
         self.list_response = []
 
-    def rqst(self, url):
+    def _rqst(self, url):
         try:
             return self.req(url, timeout=self.time_out)
 
         except Exception as e:
             print(e)
 
-    def re_search(self, tag_ini, tag_fin, input):
+    def _re_search(self, tag_ini, tag_fin, input):
         return re.search(f'{tag_ini}(.*){tag_fin}', input).group(1)
 
     #  recover query_hash, tanks: https://www.diggernaut.com/blog/
     def get_query_hash(self):
 
-        r = self.rqst(f'https://www.instagram.com/p/{self.uri}/')
+        r = self._rqst(f'https://www.instagram.com/p/{self.uri}/')
         if r:
-            ppc = self.re_search(self.tag_to_re_a_i, self.tag_to_re_a_f, r.text)
+            ppc = self._re_search(self.tag_to_re_a_i, self.tag_to_re_a_f, r.text)
             # print(ppc)
-            rp = self.rqst(f'https://www.instagram.com/static/bundles/metro/ProfilePageContainer.js/{ppc}.js')
+            rp = self._rqst(f'https://www.instagram.com/static/bundles/metro/ProfilePageContainer.js/{ppc}.js')
             if rp:
                 # print(rp.text)
-                return self.re_search(self.tag_to_re_b_i, self.tag_to_re_b_f, rp.text)
+                return self._re_search(self.tag_to_re_b_i, self.tag_to_re_b_f, rp.text)
 
             else:
                 print('unable to get query hash')
@@ -60,9 +59,8 @@ class InstaComments:
         else:
             print('was not possible to access the page')
 
-    def req_json(self, query_h, insta_uri, max_p, after=''):
-        res = requests.get(
-            f'https://www.instagram.com/graphql/query/?query_hash={query_h}&variables={{"shortcode":"{insta_uri}","first":{max_p},"after":"{after}"}}').json()
+    def _req_json(self, query_h, insta_uri, max_p, after=''):
+        res = self._rqst(f'https://www.instagram.com/graphql/query/?query_hash={query_h}&variables={{"shortcode":"{insta_uri}","first":{max_p},"after":"{after}"}}').json()
 
         if res:
             self.nova_pagina = res['data']['shortcode_media']['edge_media_to_comment']['page_info']['end_cursor']
@@ -83,12 +81,12 @@ class InstaComments:
             print(f'\nUse this query hash as parameter in "start()" function to several speed improvement!: "{q_hash}"')
 
         if q_hash:
-            if self.max_com > 0 <= 49:
-                self.req_json(q_hash, self.uri, self.max_com)
+            if self.max_com > 0 <= 40:
+                self._req_json(q_hash, self.uri, self.max_com)
 
-            if all([self.max_com > 49, self.nova_pagina]):
+            if self.nova_pagina:
                 while len(self.list_response) < self.max_com:
-                    self.req_json(q_hash, self.uri, self.max_com, self.nova_pagina)
+                    self._req_json(q_hash, self.uri, self.max_com, self.nova_pagina)
 
         # print(self.list_response)
         print(f'\nwell done! {len(self.list_response)} comments recovered!\n')
@@ -98,13 +96,13 @@ class InstaComments:
 if __name__ == '__main__':
     """https://www.instagram.com/p/B7XA4vblU4o/"""
 
-    #  Apenas isso:
+    #  just this:
 
-    post = 'B7XA4vblU4o'
-    inst = InstaComments(post, 60)
-    print(inst.start())
+    #post = 'B7XA4vblU4o'
+    #inst = InstaComments(post, 50)
+    #print(inst.start())
 
-    #  ou isso:
+    #  or:
 
     # post = 'B7XA4vblU4o'print()
     # inst = InstaComments(post, 60)
@@ -112,8 +110,8 @@ if __name__ == '__main__':
     # qh = inst.get_query_hash()
     # print(inst.start(qh))
 
-    #  ou ainda (melhor):
+    #  or even (better):
 
     # post = 'B7XA4vblU4o'
-    # inst = InstaComments(post, 60)
-    # print(inst.start("f0986789a5c5d17c2400faebf16efd0d"))
+    # inst = InstaComments(uri=post,max_com=42, timeout=5).start("f0986789a5c5d17c2400faebf16efd0d")
+    # print(inst)
